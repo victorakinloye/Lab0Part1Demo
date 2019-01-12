195fb6eed2d1f9924002eba68db471995c2a77e6
Akinloye-Victor-215592090 <46456454+victorakinloye@users.noreply.github.com>
2019-01-11 19:46:16 -0500

    Update NumberVariables.java

--- HelloWorld/src/NumberVariables.java
+++ HelloWorld/src/NumberVariables.java
@@ -1,9 +1,35 @@
-
 public class NumberVariables {
 
+
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("After intializing i's value to 5 ");
+
+		
+
+		int i=5;
+
+		System.out.println("i's value is: " + i );
+
+		
+
+		i = 7;
+
+		System.out.println("After changing i value to 7");
+
+		System.out.println("i value is: " + i);
+
+		
+
+		double x = 23.5;
+
+		double y = 24.7;
+
+		System.out.println("Average between x and y is :" +((x+y)/2));
+
+		System.out.println("Average between "+x+ "and" +y+ "is :" +((x+y)/2));
 
 	}
 
-}
+	}
