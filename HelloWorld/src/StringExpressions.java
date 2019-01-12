4ea261261ae19c314d3a9d4d4c3ac277b37e8692
Akinloye-Victor-215592090 <46456454+victorakinloye@users.noreply.github.com>
2019-01-12 00:39:31 -0500

    Add files via upload

---
+++ HelloWorld/src/StringExpressions.java
@@ -0,0 +1,44 @@
+
+public class StringExpressions {
+
+
+
+	public static void main(String[] args) {
+
+		System.out.println("We have various attempts to calulate he average between 5 and 2");
+
+		
+
+		System.out.print("5+2/2");
+
+		System.out.println("is");
+
+		System.out.println(5+2/2);
+
+		
+
+		System.out.println("(5+2)/2 is " + (5+2/2));
+
+		System.out.println("5+2/2.0 is " + (5+2/2.0) );
+
+		
+
+		System.out.println("Lassonde School of Engineering");
+
+		System.out.println("Lassonde"+" "+ "School" + " " + " of "+ " " + "Engineering");
+
+		
+
+		System.out.println("Lassonde School of Engineering (1+1): " + (1+1) );
+
+		System.out.println("Lassonde School of Engineering 1+1: " + 1+1);
+
+		
+
+		
+
+	}
+
+
+
+}
